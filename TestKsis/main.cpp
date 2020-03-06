de1b12043340e0f1016f8908f3e76e61cc59f9da
Bionchik <golper20091@rambler.ru>
2020-03-06 04:33:35 +0300

    Remarks

--- TestKsis/main.cpp
+++ TestKsis/main.cpp
@@ -17,42 +17,45 @@ int main(int argc, char *argv[])
 {
 	
 	QCoreApplication a(argc, argv);
-
-
-	QTextCodec *codec = QTextCodec::codecForName("Windows-1251");
-	QTextStream out(stdout);
-	out.setCodec(codec);
-	//QNetworkInterface Network;
 	
 	QList<QNetworkInterface> Interfaces = QNetworkInterface::allInterfaces();
 
 
 	for (int i = 0; i < Interfaces.size(); i++)
 	{
-		if (Interfaces[i].type() == QNetworkInterface::Ethernet || Interfaces[i].type() == QNetworkInterface::Wifi)
+		//QNetworkInterface::InterfaceType asd = Interfaces[i].type();
+
+		//QNetworkInterface::InterfaceFlags sda = Interfaces[i].flags();
+
+		if (Interfaces[i].type() == QNetworkInterface::Ethernet || Interfaces[i].type() == QNetworkInterface::Wifi || Interfaces[i].type() == QNetworkInterface::Unknown)
 		{
-			std::cout << Interfaces[i].humanReadableName().toStdString() << std::endl;
-			std::cout << Interfaces[i].hardwareAddress().toStdString() << std::endl;
+			std::cout << "Name            \t" << Interfaces[i].humanReadableName().toStdString() << std::endl;
+			std::cout << "Hardware address\t" << Interfaces[i].hardwareAddress().toStdString() << std::endl;
 			QList<QNetworkAddressEntry> AddressOfInterface = Interfaces[i].addressEntries();
 
-			for (int j = 0; j < AddressOfInterface.size(); j++)
+			for (int j = 1; j < AddressOfInterface.size(); j++)
 			{
-				std::cout << AddressOfInterface[j].broadcast().toString().toStdString() << std::endl;
-				std::cout << AddressOfInterface[j].ip().toString().toStdString() << std::endl;
-				std::cout << AddressOfInterface[j].netmask().toString().toStdString() << std::endl;
-
-				IPAddr DestIP = AddressOfInterface[j].ip().toIPv4Address() & AddressOfInterface[j].netmask().toIPv4Address();					
-				IPAddr SrcIP = AddressOfInterface[j].ip().toIPv4Address();
-				ULONG MacAddr[2];       /* for 6-byte hardware addresses */
-				ULONG PhysAddrLen = 6;  /* default to length of six bytes */
-
-				for (DestIP++; DestIP < AddressOfInterface[j].broadcast().toIPv4Address(); DestIP++)
+				if (Interfaces[i].flags() & QNetworkInterface::IsUp)
 				{
-					//QHostAddress host(DestIP);
-					//std::cout << host.toString().toStdString() << std::endl;
-					SendARP(DestIP, SrcIP, &MacAddr, &PhysAddrLen);
-				}				
+					std::cout << "broadcast       \t" << AddressOfInterface[j].broadcast().toString().toStdString() << std::endl;
+					std::cout << "ipV4            \t" << AddressOfInterface[j].ip().toString().toStdString() << std::endl;
+					std::cout << "Mask            \t" << AddressOfInterface[j].netmask().toString().toStdString() << std::endl;
+
+					IPAddr DestIP = AddressOfInterface[j].ip().toIPv4Address() & AddressOfInterface[j].netmask().toIPv4Address();					
+					IPAddr SrcIP = AddressOfInterface[j].ip().toIPv4Address();
+					ULONG MacAddr[2];       /* for 6-byte hardware addresses */
+					ULONG PhysAddrLen = 6;  /* default to length of six bytes */
+
+				
+					for (DestIP++; DestIP < AddressOfInterface[j].broadcast().toIPv4Address(); DestIP++)
+					{
+						//QHostAddress host(DestIP);
+						//std::cout << host.toString().toStdString() << std::endl;
+						SendARP(DestIP, SrcIP, &MacAddr, &PhysAddrLen);
+					}
+				}							
 			}
+			std::cout << "\n\n";
 		}
 	}
 
@@ -63,9 +66,10 @@ int main(int argc, char *argv[])
 	cmd.start("cmd.exe", QStringList() << "/c" << "arp -a");
 	cmd.waitForStarted();
 	cmd.waitForFinished();
-	QString ddd = cmd.readAll();
+	QString ARP = cmd.readAll();
 
-	out << QString(ddd) << endl;
+	std::cout << ARP.toStdString();
+	//out << QString(ddd) << endl;
 
 	return a.exec();
 }
